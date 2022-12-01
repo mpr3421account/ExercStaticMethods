1b22cb5350049fc295db2a78519a9acdbd417d30
Marcos Paulo <itsolutions031250@gmail.com>
2022-12-01 14:59:15 -0300

    Add project files.

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ExercStaticMethods
+{
+    internal class ConversorDeMoeda
+    {
+        public static double IOF = 6.0;
+
+        public static double Converter(double money, double cotation)
+        {
+            double total = money * cotation;
+            return total + total * IOF / 100;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por
+uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda
+que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda
+para ser responsável pelos cálculos.*/
+
+using ExercStaticMethods;
+using System.Globalization;
+
+Console.Write("Qual a cotação do dólar? ");
+double cotation = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantos dólares você vai comprar? ");
+double money = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double value = ConversorDeMoeda.Converter(cotation,money);
+
+Console.WriteLine("Valor a ser pago em reais: = " + value.ToString("F2", CultureInfo.InvariantCulture));
+
